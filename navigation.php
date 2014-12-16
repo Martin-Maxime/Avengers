00cd84d4fc055df689b0e126f1345898c516d225
ducdeswin7 <ducdeswin@outlook.com>
2014-12-16 17:51:20 +0100

    initial commit

---
+++ navigation.php
@@ -0,0 +1,11 @@
+<header>
+	<a href="" id="logoNav"><img src="img/petitLogo.png" alt="Accueil" width="40px" height="40px"></a>
+		<nav>
+            <ul>
+                <li><a href="">Accueil</a></li>
+                <li><a href="">Teaser</a></li>
+                <li><a href="">Galerie</a></li>
+                <li><a href="">Personnages</a></li>
+            </ul>
+	    </nav>
+</header>
