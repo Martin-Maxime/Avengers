c564410795cb0dee89c1a42e8bfde532aa661461
MARTIN Maxime <maximemartin45@msn.com>
2014-12-16 18:51:08 +0100

    fixes menu 2

--- navigation.php
+++ navigation.php
@@ -3,7 +3,7 @@
             <ul>
                 <li><a href="">Accueil</a></li>
                 <li><a href="">Teaser</a></li>
-                <li><a href="" id="logoNav"><img src="img/petitLogo.png" alt="Accueil" width="40px" height="40px"></a></li>
+                <li id="logoNav"><a href=""><img src="img/petitLogo.png" alt="Accueil" width="40px" height="40px"></a></li>
                 <li><a href="">Galerie</a></li>
                 <li><a href="">Personnages</a></li>
             </ul>
