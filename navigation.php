5269cd88bfc7793a8e1ce78bc77cd78e938c2c0f
MARTIN Maxime <maximemartin45@msn.com>
2014-12-16 18:02:28 +0100

    fixes navigation

--- navigation.php
+++ navigation.php
@@ -1,9 +1,9 @@
 <header>
-	<a href="" id="logoNav"><img src="img/petitLogo.png" alt="Accueil" width="40px" height="40px"></a>
 		<nav>
             <ul>
                 <li><a href="">Accueil</a></li>
                 <li><a href="">Teaser</a></li>
+                <li><a href="" id="logoNav"><img src="img/petitLogo.png" alt="Accueil" width="40px" height="40px"></a></li>
                 <li><a href="">Galerie</a></li>
                 <li><a href="">Personnages</a></li>
             </ul>
